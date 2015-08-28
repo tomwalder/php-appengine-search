fd9de396f6f65158ced53c49e5302eae2ea2bf8b
twalder-docnet <tom@doc-net.com>
2015-08-28 10:10:40 +0100

    Additional tests

---
+++ tests/QueryTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2015 Tom Walder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Tests for Query class
+ *
+ * @author Tom Walder <tom@docnet.nu>
+ */
+class QueryTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Set setting and getting the query string
+     */
+    public function testQueryString()
+    {
+        $obj_query = new \Search\Query('test string here');
+        $this->assertEquals('test string here', $obj_query->getQuery());
+    }
+
+    /**
+     * Test getting and setting the limit
+     */
+    public function testLimit()
+    {
+        $obj_query = new \Search\Query();
+        $this->assertEquals(20, $obj_query->getLimit());
+        $obj_query->limit(121);
+        $this->assertEquals(121, $obj_query->getLimit());
+        $obj_query->limit(-10);
+        $this->assertEquals(1, $obj_query->getLimit());
+        $obj_query->limit(2001);
+        $this->assertEquals(1000, $obj_query->getLimit());
+        $obj_query2 = $obj_query->limit(2001);
+        $this->assertSame($obj_query, $obj_query2);
+    }
+
+}
