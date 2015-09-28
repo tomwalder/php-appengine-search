3b61caaf820052808a00b163362e27a16e414e92
twalder-docnet <tom@doc-net.com>
2015-09-28 22:25:35 +0100

    Add more Index tests

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -20,7 +20,7 @@
  *
  * @author Tom Walder <tom@docnet.nu>
  */
-class IndexTest extends \PHPUnit_Framework_TestCase
+class IndexTest extends \google\appengine\testing\ApiProxyTestBase
 {
 
     /**
@@ -30,6 +30,160 @@ class IndexTest extends \PHPUnit_Framework_TestCase
     {
         $obj_index = new \Search\Index('some-index');
         $this->assertInstanceOf('\\Search\\Index', $obj_index);
+    }
+
+    /**
+     * Test put failure
+     *
+     * @expectedException        InvalidArgumentException
+     * @expectedExceptionMessage Parameter must be one or more \Search\Document objects
+     */
+    public function testPutTypeFailure()
+    {
+        $obj_index = new \Search\Index('test');
+        $obj_index->put('error-string');
+    }
+
+    /**
+     * Basic search test
+     */
+    public function testBasicSearch()
+    {
+        $str_index = 'test-index';
+        $str_query = 'phrase';
+
+        $obj_request = new \google\appengine\SearchRequest();
+        $obj_request->mutableParams()
+            ->setQuery($str_query)
+            ->setLimit(20)
+            ->setOffset(0)
+            ->mutableIndexSpec()->setName($str_index);
+
+        $this->apiProxyMock->expectCall('search', 'Search', $obj_request, new \google\appengine\SearchResponse());
+        $obj_index = new \Search\Index($str_index);
+        $obj_index->search($str_query);
+        $this->apiProxyMock->verify();
+    }
+
+    /**
+     * Basic search test
+     */
+    public function testQuerySearch()
+    {
+        $str_index = 'test-index';
+        $str_query = 'phrase';
+
+        $obj_request = new \google\appengine\SearchRequest();
+        $obj_request->mutableParams()
+            ->setQuery($str_query)
+            ->setLimit(20)
+            ->setOffset(0)
+            ->mutableIndexSpec()->setName($str_index);
+
+        $this->apiProxyMock->expectCall('search', 'Search', $obj_request, new \google\appengine\SearchResponse());
+        $obj_index = new \Search\Index($str_index);
+        $obj_index->search(new \Search\Query($str_query));
+        $this->apiProxyMock->verify();
+    }
+
+    /**
+     * Test get by ID. Also ensure we can access the last request and response objects.
+     */
+    public function testGetById()
+    {
+        $str_index = 'test-index';
+        $str_id = 'abc123def456';
+
+        $obj_request = new \google\appengine\ListDocumentsRequest();
+        $obj_params = $obj_request->mutableParams();
+        $obj_params->mutableIndexSpec()->setName($str_index);
+        $obj_params->setStartDocId($str_id)->setLimit(1);
+        $obj_response = new \google\appengine\ListDocumentsResponse();
+
+        $this->apiProxyMock->expectCall('search', 'ListDocuments', $obj_request, $obj_response);
+        $obj_index = new \Search\Index($str_index);
+        $obj_index->get($str_id);
+        $this->apiProxyMock->verify();
+
+        $arr_debug = $obj_index->debug();
+        $this->assertInstanceOf('\\google\\appengine\\ListDocumentsRequest', $arr_debug[0]);
+        $this->assertInstanceOf('\\google\\appengine\\ListDocumentsResponse', $arr_debug[1]);
+    }
+
+    /**
+     * Test the delete document function
+     */
+    public function testDeleteArrayIdStrings()
+    {
+        $str_index = 'test-index';
+        $arr_ids = ['123456789', 'abc123'];
+
+        $obj_request = new \google\appengine\DeleteDocumentRequest();
+        $obj_params = $obj_request->mutableParams();
+        $obj_params->mutableIndexSpec()->setName($str_index);
+        foreach($arr_ids as $str_id) {
+            $obj_params->addDocId($str_id);
+        }
+
+        $this->apiProxyMock->expectCall('search', 'DeleteDocument', $obj_request, new \google\appengine\DeleteDocumentResponse());
+        $obj_index = new \Search\Index($str_index);
+        $obj_index->delete($arr_ids);
+        $this->apiProxyMock->verify();
+    }
+
+    /**
+     * Test the delete document function with a variety of inputs
+     */
+    public function testDeleteMulti()
+    {
+        $str_index = 'test-index';
+        $str_id = '123456789';
+
+        $obj_request = new \google\appengine\DeleteDocumentRequest();
+        $obj_params = $obj_request->mutableParams();
+        $obj_params->mutableIndexSpec()->setName($str_index);
+        $obj_params->addDocId($str_id);
+
+        $this->apiProxyMock->expectCall('search', 'DeleteDocument', $obj_request, new \google\appengine\DeleteDocumentResponse());
+        $obj_index = new \Search\Index($str_index);
+        $obj_index->delete($str_id);
+        $this->apiProxyMock->verify();
+
+        $this->apiProxyMock->expectCall('search', 'DeleteDocument', $obj_request, new \google\appengine\DeleteDocumentResponse());
+        $obj_doc = new \Search\Document();
+        $obj_doc->setId($str_id);
+        $obj_index->delete($obj_doc);
+        $this->apiProxyMock->verify();
+
+        $this->apiProxyMock->expectCall('search', 'DeleteDocument', $obj_request, new \google\appengine\DeleteDocumentResponse());
+        $obj_doc = new \Search\Document();
+        $obj_doc->setId($str_id);
+        $obj_index->delete([$obj_doc]);
+        $this->apiProxyMock->verify();
+    }
+
+    /**
+     * Test the delete document function failure
+     *
+     * @expectedException        InvalidArgumentException
+     * @expectedExceptionMessage Parameter must be one or more \Search\Document objects
+     */
+    public function testDeleteFailure()
+    {
+        $obj_index = new \Search\Index('test');
+        $obj_index->delete(123);
+    }
+
+    /**
+     * Test the delete document function failure
+     *
+     * @expectedException        InvalidArgumentException
+     * @expectedExceptionMessage Parameter must be one or more \Search\Document objects
+     */
+    public function testDeleteFailureArray()
+    {
+        $obj_index = new \Search\Index('test');
+        $obj_index->delete(['abc', 123]);
     }
 
 }
